c3cca89172bd3530bd710fd55410a91f66ad3f63
accomazzo <mikey@accomazzo.net>
2015-08-04 10:08:32 -0700

    My Front End Challenge
    Used bootstrap and bit of simple jQuery to add the functionality needed.
    See inline comments for more information.

--- script.js
+++ script.js
@@ -21,11 +21,38 @@ function domobj(){
     var i=0
     thishtml='';
     for( i=0; i< self.products.length ; i++){
-      if (i % 3 == 0 ){  thishtml += "<div class='row'>"; console.log("START") }
       thishtml += self.products[i].htmlview;
-      if ((i % 3 == 2) || i == (self.products.length-1) ){thishtml += "</div>";console.log("FINISH")}
-    }
+      }
     $("#content").append(thishtml)
+  }
+  
+//  added simple jQuery here for removal and overlay functionality
+    self.stylizeContent = function () {
+   
+    $( ".remove" ).click(function() {
+        //caching this for better performance
+        var $this = $(this);
+        var parentDiv = $this.parents('.col-sm-4');
+         parentDiv.slideUp(200, function() {   
+         parentDiv.attr("style", "display:none; position: absolute;");
+        });
+    });
+          
+    $( ".product-container" ).hover(function() {
+        var $this = $(this);
+        
+        //make overlay same size as .product-container
+        var prodHeight = $this.height();
+        var prodWidth = $this.width();
+        $this.find(".info").height(prodHeight).width(prodWidth);
+        
+        $this.find(".info").toggleClass("info-hover");
+        $this.find("img").toggleClass("img-hover");
+
+    });
+    
+    
+    
   }
   
 }
@@ -39,16 +66,19 @@ function productobj(product, i){
   self.htmlview     = ""
   self.index        = i
   self.custom_class = "col"+ ((i % 3) +1)
+  self.description  = product.description
   
   self.updatehtml= function(){
     $.get('product-template.html', function(template){
-      self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{custom_class}', self.custom_class);
+      self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{custom_class}', self.custom_class).replace('{info}', self.description);
     });
   }
 }
 
-
+//would ideally remove setTimeout with callbacks, will do in a future revision time permititng
 var page=new domobj();
-page.getproducts('data.json');
-setTimeout("console.log('building html');page.updateproducthtml();",20);
-setTimeout("page.updatedom()",50)
+page.getproducts('data-orig.json');
+setTimeout("page.updateproducthtml();",400);
+setTimeout("page.updatedom()",1000);
+setTimeout("page.stylizeContent()", 1200);
+setTimeout("$('.container').attr('style', 'display:block;')", 1400);
